ce0b4eaa097e433e3623763ed8d12ed7a23c7598
Anna evstafa <a.evstafa@gmail.com>
2022-12-16 19:43:04 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        long x = 4000;
+        int i = 20;
+                i = (int) x / i;
+        System.out.println(i);
+    }
+}
